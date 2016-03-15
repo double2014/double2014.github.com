f9ade8361d597b2b0fc7a18944a2ed55c3900bf0
double2014 <1283268128@qq.com>
2016-03-15 16:40:37 +0800

    update file

--- skin/js/scrolltop.js
+++ skin/js/scrolltop.js
@@ -1,18 +1,51 @@
+
+
 // JavaScript Document
-function goTopEx(){
-        var obj=document.getElementById("topcontrol");
-        function getScrollTop(){
-                return document.documentElement.scrollTop;
+
+var goTopEx=function (){
+	var obj=document.getElementById("topcontrol");
+
+	function addEvent(obj,type,fn){
+    if(obj.attachEvent){
+        obj.attachEvent('on'+type,function(){
+            fn.call(obj);
+        })
+    }else{
+        obj.addEventListener(type,fn,false);
+    }
+}
+
+function getScrollTop(){
+                return document.documentElement.scrollTop||document.body.scrollTop;
             }
-        function setScrollTop(value){
-                document.documentElement.scrollTop=value;
-            }    
-        window.onscroll=function(){getScrollTop()>0?obj.style.display="":obj.style.display="none";}
-        obj.onclick=function(){
+
+
+function setScrollTop(value){
+				if(document.documentElement.scrollTop){
+					document.documentElement.scrollTop=value;
+				}else if(document.body.scrollTop){
+					document.body.scrollTop=value;
+				}
+            } 
+
+        addEvent(window,'scroll',function(){
+			getScrollTop()>0?obj.style.display="":obj.style.display="none";
+		});
+        addEvent(obj,'click',function(){
+			var goTop=setInterval(scrollMove,10);
+            function scrollMove(){
+                    setScrollTop(getScrollTop()/1.1);
+                    if(getScrollTop()<1)clearInterval(goTop);
+                }
+		});  
+       // window.onscroll=function(){getScrollTop()>0?obj.style.display="":obj.style.display="none";}
+	   /*obj.onclick=function(){
             var goTop=setInterval(scrollMove,10);
             function scrollMove(){
                     setScrollTop(getScrollTop()/1.1);
                     if(getScrollTop()<1)clearInterval(goTop);
                 }
-        }
+        }*/
+	   
+        
     }
